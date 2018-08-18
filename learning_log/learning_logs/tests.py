42733b89fe758b6003789d66dd5b79fdcb9eddf4
tianquanyou <quanyou.t@gmail.com>
2018-08-18 11:30:34 +0800

    list

--- learning_log/learning_logs/tests.py
+++ learning_log/learning_logs/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+suffixes = {1000 : ['KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
+            1024 : ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB']
+            }
+
+def approximate_size(size, a_kilobyte_is_1024_bytes=True):
+
+    if size < 0:
+        raise ValueError('number must be non-negative')
+
+
+    multiple = 1024 if a_kilobyte_is_1024_bytes else 1000
+
+    for suffix in suffixes[multiple]:
+
+        size = size / multiple
+
+        if size < multiple:
+            return '{0:.1f} {1}'.format(size, suffix)
+
+
+    raise VauleError('number too large')
+
+
+if __name__ == '__main__':
+
+    print(approximate_size(10000000000000000, False))
+    print(approximate_size(10000000000000))
